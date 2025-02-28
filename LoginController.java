fc56c214266afcf5c09eafdcd2fd325ac79195b6
Calo08 <122736164+Calo08@users.noreply.github.com>
2025-02-28 10:32:26 +0200

    LoginController.java

--- LoginController.java
+++ LoginController.java
@@ -1,8 +1,14 @@
 package controllers;
 
+import dao.UserDAO;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import java.io.IOException;
 
 public class LoginController {
     @FXML private TextField usernameField;
@@ -13,9 +19,22 @@ public class LoginController {
         String username = usernameField.getText();
         String password = passwordField.getText();
 
-        if (username.equals("admin") && password.equals("admin123")) {
+        if (UserDAO.validateUser(username, password)) {
             System.out.println("Login successful!");
-            // Proceed to dashboard (to be implemented)
+
+            try {
+                FXMLLoader loader = new FXMLLoader(getClass().getResource("/views/Dashboard.fxml"));
+                Parent root = loader.load();
+                Stage stage = new Stage();
+                stage.setTitle("Dashboard");
+                stage.setScene(new Scene(root, 800, 600));
+                stage.show();
+
+                Stage currentStage = (Stage) usernameField.getScene().getWindow();
+                currentStage.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         } else {
             System.out.println("Invalid credentials!");
         }
